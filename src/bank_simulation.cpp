28b55d629e9c11d653e7fe3ddefe15c037289ebd
Colin <colinhaskins2022@gmail.com>
2025-02-26 17:31:46 -0500

    added to bank sim

--- src/bank_simulation.cpp
+++ src/bank_simulation.cpp
@@ -19,29 +19,72 @@ class BankAccount{
             // critical section
             Balance -= amount;
             // delay inside critical section to simulate work
-            std::this_thread::sleep_for(std::chrono::milliseconds(20));
+            std::this_thread::sleep_for(std::chrono::milliseconds(3000));
             mtx.unlock();
-            std::cout << "[Account " << ID << "]" << amount << "$ widthdrawl sucessful" << std::endl;
-            std::cout << "[Account " << ID << "]" << " Current Balance: " << Balance << std::endl;
+            std::cout << "                          |   [Account " << ID << "]" << amount << "$ widthdrawl sucessful" << std::endl;
+            std::cout << "                          |   [Account " << ID << "]" << " Current Balance: " << Balance << std::endl;
             return true;
         }
 
-        std::cout << "[Account " << ID << "]" << amount << "$ widthdrawl failed" << std::endl;
+        std::cout << "                          |   [Account " << ID << "]" << amount << "$ widthdrawl failed, other modification in process" << std::endl;
         return false;
 
+    }
+
+    bool deposit(int amount) {
+    if (mtx.try_lock()){
+        // critical section
+        Balance += amount;
+        // delay inside critical section to simulate work
+        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+        mtx.unlock();
+        std::cout << "                          |   [Account " << ID << "]" << amount << "$ deposit sucessful" << std::endl;
+        std::cout << "                          |   [Account " << ID << "]" << " Current Balance: " << Balance << std::endl;
+        return true;
+    }
+
+    std::cout << "                          |   [Account " << ID << "]" << amount << "$ widthdrawl failed, other modification in process" << std::endl;
+    return false;
     }
 
 };
 
 int main() {
+    bool end = false;
+    BankAccount acc1(1000, 1);
+    BankAccount acc2(1000, 2);
+    while (!end){
+        int choice;
+        std::cout << "[Bank Account Operations] |" << std::endl;
+        std::cout << "Widthdrawal  -------- 0   |" << std::endl;
+        std::cout << "Deposit      -------- 1   |" << std::endl;
+        std::cout << "Transfer     -------- 2   |" << std::endl;
+        std::cout << "==========================|" << std::endl;
+        std::cout << "[Simulate Deadlock] - 3   |" << std::endl;
+        std::cout << "[Resolve Deadlock] -- 4   |" << std::endl;
+        std::cout << "                          |" << std::endl;
+        std::cout << "Enter choice:             |\n";
+        std::cin >> choice;
+        if (choice == 0){
+            int amt;
+            std::cout << "Enter Widthdrawal amount: |\n";
+            std::cin >> amt;
 
-    BankAccount acc1(1000, 0);
 
-    std::thread t1(&BankAccount::withdraw, &acc1, 10);
-    std::thread t2(&BankAccount::withdraw, &acc1, 10);
+            std::thread t1(&BankAccount::withdraw, &acc1, amt);
+            t1.detach();
+        } else if (choice == 1){
+            int amt;
+            std::cout << "Enter Deposit amount:     |\n";
+            std::cin >> amt;
 
-    t1.join();
-    t2.join();
+
+            std::thread t1(&BankAccount::deposit, &acc1, amt);
+            t1.detach();
+        }
+        
+    }
+
 
     return 0;
 }
